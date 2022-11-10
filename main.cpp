d706addf1c67fa5f8989fc8d1968f9f9c1037de7
domgudas <114651870+domgudas@users.noreply.github.com>
2022-11-10 20:00:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,131 +1,145 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <list>
+#include <vector>
 #include <iomanip>
 #include <cmath>
 #include <cstdlib>
+#include <stdlib.h>
 #include <bits/stdc++.h>
+#include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
-struct Studentai{
-    string Var[100];
-    string pVar[100];
-    float nd[100][10];
-    float egz[100];
-    float galut[100];
-    double counter[100];
-};
-
 int main(){
-    int nrStud;
-    int nrNd;
-    char arND;
-    char MarV;
-    char atsit;
-    Studentai stud;
 
-    cout << "Iveskite studentu skaiciu" << endl;
-    cin  >> nrStud;
-    cout << "Jei namu darbu kieki zinote is anksto, irasykite z, o jei nezinote, irasykite n" << endl;
-    cin  >> arND;
-    cout << "Jei noresite viska pateikta mediana, irasykite m, o jei vidurkiu, irasykite v" << endl;
-    cin  >> MarV;
+    vector <string> Var;
+    vector <string> pVar;
+    vector <float> galut;
+    list <string> LVar;
+    list <string> LpVar;
+    list <float> Lgalut;
 
-    if(arND == 'z'){ // namu darbu kiekis is anksto zinomas 
-        cout << "Iveskite namu darbu skaiciu" << endl;
-        cin  >> nrNd;
-        cout << "Ar norite atsitiktinai sugeneruoti egzamino ir namu darbu balus? Jei taip, rasykite t, o jei ne, rasykite n" << endl;
-        cin  >> atsit;
+    string Vardas, pVardas;
+    float galutinis;
 
-        if(atsit == 'n'){ // neatsitiktinai 
-            for(int i = 0; i < nrStud; i++){
-                cout << "Iveskite " << i+1 << " studento varda" << endl;
-                cin  >> stud.Var[i];
-                cout << "Iveskite " << i+1 << " studento pavarde" << endl;
-                cin  >> stud.pVar[i];
-                cout << "Iveskite " << i+1 << " studento egzamino bala" << endl;
-                cin  >> stud.egz[i];
+    int stSk;
     
-                for(int j = 0; j < nrNd; j++){
-                    cout << "Iveskite " << i+1 << " studento " << j+1 << "namu darbu bala" << endl;
-                    cin  >> stud.nd[i][j];
-                }
-            }
+    cout << "Iveskite studentu skaiciu" << endl;
+    cin  >> stSk;
+
+    ofstream out("StudentaiRandom.txt"); // failo sukurimas
+
+    for(int i = 0; i < stSk; i++){
+        out << "Vardenis" << i+1 << " Pavardenis" << i+1 << " " << rand() % 10 + 1 << endl;
+    }
+
+    out.close();
+
+    chrono::steady_clock::time_point b1 = std::chrono::steady_clock::now();
+
+    ifstream in("StudentaiRandom.txt"); // failo nuskaitymas VECTOR
+
+    for(int i = 0; i < stSk; i++){
+        in >> Vardas >> pVardas >> galutinis;
+
+        Var.push_back(Vardas);
+        pVar.push_back(pVardas);
+        galut.push_back(galutinis);
+    }
+
+    in.close();
+
+    std::chrono::steady_clock::time_point e1 = std::chrono::steady_clock::now();
+
+    double laikas = (chrono::duration_cast<std::chrono::nanoseconds>(e1 - b1).count())/1000000000.0;
+
+    double sum = sum + laikas;
+
+    cout << "Failo nuskaitymas vektoriais uztruko: " << laikas << " sekundziu." << endl;
+
+    chrono::steady_clock::time_point b2 = std::chrono::steady_clock::now();
+
+    ofstream out1("GalvociaiV.txt"); // studentu isskirstymas VECTOR
+    ofstream out2("VargsiukaiV.txt");
+
+    for(int i = 0; i < stSk; i++){
+        if(galut[i] < 5){
+            out2 << Var[i] << " " << pVar[i] << " " << galut[i] << endl;
         }
-            else if (atsit == 't'){ // atsitiktinai generuojami skaiciai
-                for(int i = 0; i < nrStud; i++){
-                    cout << "Iveskite " << i+1 << " studento varda" << endl;
-                    cin  >> stud.Var[i];
-                    cout << "Iveskite " << i+1 << " studento pavarde" << endl;
-                    cin  >> stud.pVar[i];
-                    
-                    stud.egz[i] = rand() % 10 + 1;
-                    for(int j = 0; j < nrNd; j++){
-                        stud.nd[i][j] = rand() % 10 + 1;
-                    }
-                }
+            else if (galut[i] >= 5){
+                out1 << Var[i] << " " << pVar[i] << " " << galut[i] << endl;
             }
     }
-        else if(arND == 'n'){ // namu darbu kiekis vedant pasakomas
-            for(int i = 0; i < nrStud; i++){
-                cout << "Iveskite " << i+1 << " studento varda" << endl;
-                cin  >> stud.Var[i];
-                cout << "Iveskite " << i+1 << " studento pavarde" << endl;
-                cin  >> stud.pVar[i];
-                cout << "Iveskite " << i+1 << " studento egzamino bala" << endl;
-                cin  >> stud.egz[i];
 
-                stud.counter[i] = 0;
-                for(int j = 0; j < 10000; j++){
-                    cout << "Iveskite " << i+1 << "-o studento, " << j+1 << "-o namu darbu bala. Kai noresite baigti, irasykite -1" << endl;
-                    cin  >> stud.nd[i][j];
+    out1.close();
+    out2.close();
 
-                    if(stud.nd[i][j] == -1){
-                        break;
-                    }
-                        else {
-                            stud.counter[i] = stud.counter[i] + 1;
-                        }
-                }
-            }
-        }
+    chrono::steady_clock::time_point e2 = std::chrono::steady_clock::now();
 
-    if(MarV == 'v'){ // skaiciuojamas vidurkis
-        for(int i = 0; i < nrStud; i++){
-            float sumND = 0;
+    laikas = (chrono::duration_cast<std::chrono::nanoseconds>(e2 - b2).count())/1000000000.0;
 
-            for(int j = 0; j < nrNd || j < stud.counter[i]; j++){
-                sumND = sumND + stud.nd[i][j];
-            }
-            if(arND == 'z'){
-                stud.galut[i] = 0.4 * sumND/nrNd + 0.6 * stud.egz[i];
-            }
-                else if(arND == 'n'){
-                    stud.galut[i] = 0.4 * sumND/stud.counter[i] + 0.6 * stud.egz[i];
-                }
-        }
-    } 
-        else if (MarV == 'm'){ // skaiciuojama mediana
-            for(int i = 0; i < nrStud; i++){
-                int c = stud.counter[i];
-                if(c % 2 == 0){
-                    stud.galut[i] = 0.4 * (stud.nd[i][c/2-1]+stud.nd[i][c/2])/2 + 0.6 * stud.egz[i];
-                    cout << (stud.nd[i][c/2-1]+stud.nd[i][c/2])/2 << endl;
-                }
-                    else {
-                        double dydis2 = stud.counter[i]/2;
-                        int dydis3 = round(dydis2-1);
-                        cout << stud.counter[i] << endl;
-                        stud.galut[i] = 0.4 * stud.nd[i][dydis3] + 0.6 * stud.egz[i];
-                    }
-            }
-        }
+    sum = sum + laikas;
 
-    cout << "Pavarde                     Vardas                     Galutinis" << endl;
-    cout << "----------------------------------------------------------------" << endl;
+    cout << "Failo isrusiavimas vektoriais ir ivedimas i naujus du failus uztruko: " << laikas << " sekundziu." << endl;
+    cout << "Visi veiksmai sudejus uztruko: " << sum << " sekundziu." << endl;
 
-    for(int i = 0; i < nrStud; i++){ // isvedamas rezultatas
-        cout << stud.Var[i] << "                     " << stud.pVar[i] << "                     " << setprecision(3) << stud.galut[i] << endl;
+    chrono::steady_clock::time_point b1 = std::chrono::steady_clock::now();
+
+    // ------------------------------------------------------------------------------------------------------------------------------
+
+    sum = 0;
+    laikas = 0;
+
+    ifstream in("StudentaiRandom.txt"); // failo nuskaitymas LIST
+
+    for(int i = 0; i < stSk; i++){
+        in >> Vardas >> pVardas >> galutinis;
+
+        LVar.push_back(Vardas);
+        LpVar.push_back(pVardas);
+        Lgalut.push_back(galutinis);
     }
+
+    in.close();
+
+    std::chrono::steady_clock::time_point e1 = std::chrono::steady_clock::now();
+
+    double laikas = (chrono::duration_cast<std::chrono::nanoseconds>(e1 - b1).count())/1000000000.0;
+
+    double sum = sum + laikas;
+
+    cout << "Failo nuskaitymas list uztruko: " << laikas << " sekundziu." << endl;
+
+    chrono::steady_clock::time_point b2 = std::chrono::steady_clock::now();
+
+    ofstream out1("GalvociaiL.txt"); // studentu isskirstymas LIST
+    ofstream out2("VargsiukaiL.txt");
+
+    for(float i = 0; i < stSk; i++){
+        if(Lgalut[i] < 5){
+            out2 << LVar[i] << " " << LpVar[i] << " " << Lgalut[i] << endl;
+        }
+            else if (galut[i] >= 5){
+                out1 << LVar[i] << " " << LpVar[i] << " " << Lgalut[i] << endl;
+            }
+    }
+
+    out1.close();
+    out2.close();
+
+    chrono::steady_clock::time_point e2 = std::chrono::steady_clock::now();
+
+    laikas = (chrono::duration_cast<std::chrono::nanoseconds>(e2 - b2).count())/1000000000.0;
+
+    sum = sum + laikas;
+
+    cout << "Failo isrusiavimas list ir ivedimas i naujus du failus uztruko: " << laikas << " sekundziu." << endl;
+    cout << "Visi veiksmai sudejus uztruko: " << sum << " sekundziu." << endl;
+
+    chrono::steady_clock::time_point b1 = std::chrono::steady_clock::now();
+
     return 0;
 }
