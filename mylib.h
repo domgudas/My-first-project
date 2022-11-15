eec5880c62973a9790610a8c1137b01ca4d3954a
domgudas <114651870+domgudas@users.noreply.github.com>
2022-11-15 19:21:26 +0200

    Create mylib.h

---
+++ mylib.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <chrono>
+#include <unistd.h>
+
+using namespace std;
